76fab4d1d34870b224d726999f922db0f7b9ba9c
Luka <nikuradzeluka0@gmail.com>
2022-08-22 10:16:27 +0400

    data pass

--- app/src/main/java/com/example/tbc_course_22/ui/main/authentication/home/HomeFragment.kt
+++ app/src/main/java/com/example/tbc_course_22/ui/main/authentication/home/HomeFragment.kt
@@ -2,26 +2,49 @@ package com.example.tbc_course_22.ui.main.authentication.home
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.Navigation
 import com.example.tbc_course_22.R
+import com.example.tbc_course_22.databinding.FragmentHomeBinding
+import com.example.tbc_course_22.databinding.FragmentRegisterBinding
 
 class HomeFragment : Fragment() {
 
 
     private lateinit var viewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setFragmentResultListener("requestString"){key,bundle ->
+            val result = bundle.getString("bundleKeyString")
+            binding.textView2.text = result
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.logOut.setOnClickListener {
+            Navigation.findNavController(it).popBackStack()
+        }
+
+
     }
 
 }
--- app/src/main/java/com/example/tbc_course_22/ui/main/authentication/login/MainFragment.kt
+++ app/src/main/java/com/example/tbc_course_22/ui/main/authentication/login/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -80,6 +82,8 @@ class MainFragment : Fragment() {
                                 Snackbar.LENGTH_SHORT
                             ).show()
                             findNavController().navigate(MainFragmentDirections.actionMainFragmentToHomeFragment())
+                            val result = binding.emailEditText.text.toString()
+                            setFragmentResult("requestString", bundleOf("bundleKeyString" to result))
                         }
                     }
                 }
